0bfde833ffff46d48a80126988f6f867dcded591
Hantowmey <hantome4@gmail.com>
2025-06-08 10:52:54 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-
+{
+  "name": "bot2i_general",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "description": ""
+}
